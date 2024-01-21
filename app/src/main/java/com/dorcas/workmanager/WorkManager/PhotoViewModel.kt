0379af274c9c28679edd05e4e716ac32a6cb86c7
Dorcas owolabi <owolabidorcas40@gmail.com>
2024-01-21 18:32:17 +0100

    Initial commit

---
+++ app/src/main/java/com/dorcas/workmanager/WorkManager/PhotoViewModel.kt
@@ -0,0 +1,35 @@
+package com.dorcas.workmanager.WorkManager
+
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Parcelable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import java.util.UUID
+
+class PhotoViewModel: ViewModel() {
+    var uncompressedUri: Uri? by mutableStateOf(null)
+        private set
+
+    var compressedBitmap: Bitmap? by mutableStateOf(null)
+        private set
+
+    var workId: UUID? by mutableStateOf(null)
+        private set
+
+    fun updateUncompressUri(uri:Uri) {
+        uncompressedUri = uri
+    }
+
+    fun updateCompressBitmap(bmp: Bitmap?) {
+        compressedBitmap = bmp
+    }
+
+    fun updateWorkId(uuid: UUID?) {
+        workId = uuid
+    }
+
+
+    }
